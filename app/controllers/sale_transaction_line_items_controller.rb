db14b802fd5ec2168e7aa1d236a57a9321be1661
lxin42140 <lxin42140@gmail.com>
2021-05-11 18:09:48 +0800

    added HTML form validation

--- app/controllers/sale_transaction_line_items_controller.rb
+++ app/controllers/sale_transaction_line_items_controller.rb
@@ -3,67 +3,77 @@ class SaleTransactionLineItemsController < ApplicationController
 
   # GET /sale_transaction_line_items or /sale_transaction_line_items.json
   def index
+    begin
+      if User.isCustomer != true
+        redirect_to(root_path, notice: 'No access right - Customers only!') 
+        return
+      end
+      rescue NullPointerException
+        redirect_to "/users/sign_in", notice: 'Please sign in!'
+        return
+    end 
+    
     @sale_transaction_line_items = SaleTransactionLineItem.all
   end
 
-  # GET /sale_transaction_line_items/1 or /sale_transaction_line_items/1.json
-  def show
-  end
+  # # GET /sale_transaction_line_items/1 or /sale_transaction_line_items/1.json
+  # def show
+  # end
 
-  # GET /sale_transaction_line_items/new
-  def new
-    @sale_transaction_line_item = SaleTransactionLineItem.new
-  end
+  # # GET /sale_transaction_line_items/new
+  # def new
+  #   @sale_transaction_line_item = SaleTransactionLineItem.new
+  # end
 
-  # GET /sale_transaction_line_items/1/edit
-  def edit
-  end
+  # # GET /sale_transaction_line_items/1/edit
+  # def edit
+  # end
 
-  # POST /sale_transaction_line_items or /sale_transaction_line_items.json
-  def create
-    @sale_transaction_line_item = SaleTransactionLineItem.new(sale_transaction_line_item_params)
+  # # POST /sale_transaction_line_items or /sale_transaction_line_items.json
+  # def create
+  #   @sale_transaction_line_item = SaleTransactionLineItem.new(sale_transaction_line_item_params)
 
-    respond_to do |format|
-      if @sale_transaction_line_item.save
-        format.html { redirect_to @sale_transaction_line_item, notice: "Sale transaction line item was successfully created." }
-        format.json { render :show, status: :created, location: @sale_transaction_line_item }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @sale_transaction_line_item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @sale_transaction_line_item.save
+  #       format.html { redirect_to @sale_transaction_line_item, notice: "Sale transaction line item was successfully created." }
+  #       format.json { render :show, status: :created, location: @sale_transaction_line_item }
+  #     else
+  #       format.html { render :new, status: :unprocessable_entity }
+  #       format.json { render json: @sale_transaction_line_item.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # PATCH/PUT /sale_transaction_line_items/1 or /sale_transaction_line_items/1.json
-  def update
-    respond_to do |format|
-      if @sale_transaction_line_item.update(sale_transaction_line_item_params)
-        format.html { redirect_to @sale_transaction_line_item, notice: "Sale transaction line item was successfully updated." }
-        format.json { render :show, status: :ok, location: @sale_transaction_line_item }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @sale_transaction_line_item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # # PATCH/PUT /sale_transaction_line_items/1 or /sale_transaction_line_items/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @sale_transaction_line_item.update(sale_transaction_line_item_params)
+  #       format.html { redirect_to @sale_transaction_line_item, notice: "Sale transaction line item was successfully updated." }
+  #       format.json { render :show, status: :ok, location: @sale_transaction_line_item }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @sale_transaction_line_item.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # DELETE /sale_transaction_line_items/1 or /sale_transaction_line_items/1.json
-  def destroy
-    @sale_transaction_line_item.destroy
-    respond_to do |format|
-      format.html { redirect_to sale_transaction_line_items_url, notice: "Sale transaction line item was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+  # # DELETE /sale_transaction_line_items/1 or /sale_transaction_line_items/1.json
+  # def destroy
+  #   @sale_transaction_line_item.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to sale_transaction_line_items_url, notice: "Sale transaction line item was successfully destroyed." }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_sale_transaction_line_item
-      @sale_transaction_line_item = SaleTransactionLineItem.find(params[:id])
-    end
+  # private
+  #   # Use callbacks to share common setup or constraints between actions.
+  #   def set_sale_transaction_line_item
+  #     @sale_transaction_line_item = SaleTransactionLineItem.find(params[:id])
+  #   end
 
-    # Only allow a list of trusted parameters through.
-    def sale_transaction_line_item_params
-      params.require(:sale_transaction_line_item).permit(:item_id, :quantity, :subtotal, :decimal)
-    end
+  #   # Only allow a list of trusted parameters through.
+  #   def sale_transaction_line_item_params
+  #     params.require(:sale_transaction_line_item).permit(:item_id, :quantity, :subtotal, :decimal)
+  #   end
 end
