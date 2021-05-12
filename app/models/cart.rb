e391afd698511027d216d0c7e01fcc37519dd06e
lxin42140 <lxin42140@gmail.com>
2021-05-12 14:44:04 +0800

    added error handling and cart operations

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -12,4 +12,9 @@ class Cart < ApplicationRecord
         end
         current_item
     end
+
+    def total_price
+        sale_transaction_line_items.to_a.sum { |item| item.total_price }
+    end
+        
 end
--- app/models/sale_transaction_line_item.rb
+++ app/models/sale_transaction_line_item.rb
@@ -6,4 +6,9 @@ class SaleTransactionLineItem < ApplicationRecord
     validates :quantity, :subtotal, presence: true
     validates :subtotal, numericality: {greater_than_or_equal_to: 0.01}
     validates :quantity, numericality: {greater_than: 0}
+
+    def total_price
+        product.unit_price * quantity
+    end
+        
 end
